4e36bb3dd4509454fce01d8c0ddfe9bc6558d400
juanjose <juan.guerrero@avantica.net>
2018-02-22 16:57:51 -0600

    n5

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 8000;
 
 app.use(
   bodyParser.urlencoded({
-    extended: true
+    extended: false
   })
 );
 
