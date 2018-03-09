c545eddc91c4ad219516b975848f275fa5dedc98
juanjose <juan.guerrero@avantica.net>
2018-03-08 18:10:14 -0600

    Heroku deploy 15 test authenticate

--- routes/index.js
+++ routes/index.js
@@ -10,13 +10,13 @@ var axios = require('axios');
 //  */
 router.post("/botSpeak", (req, res) => {
   var action = req.body.result && req.body.result.action ? req.body.result.action : '';
-  console.log(req.body);
-  console.log(req.body.sessionId);
   console.log('Action : ' + action);
+  console.log(req.body);
   console.log(req);
   return res.json({
     speech: 'TEST SESSION',
     displayText: 'TEST SESSION',
+    req : req,
     source: "dialog-server-flow"
   });
   //CHECK FOR LOGIN
