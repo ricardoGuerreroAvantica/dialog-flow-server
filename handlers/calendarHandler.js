1eb0c7076177d8f5f270b29d4155ee6e2f0de197
juanjose <juan.guerrero@avantica.net>
2018-04-19 11:46:45 -0600

    Fix code 71

--- handlers/calendarHandler.js
+++ handlers/calendarHandler.js
@@ -25,17 +25,16 @@ function findMeetingTimes(options, callback){
       options.message, options.speech = "Sorry couldn't find any space";
       callback(options);
     }else{
-      options.message = `I found some space, look at these: \n\n` +
-          `--------------------------------- \n\n` +
-          `${meetings.map(meeting => `${commons.parseDate(meeting.meetingTimeSlot.start.dateTime)} - ${commons.parseDate(meeting.meetingTimeSlot.end.dateTime)} \n\n`)}`;
-
-      options.speech = 'I found some space, look at these';
-      console.log('findMeetingTimes.options : ' + JSON.stringify(options, null, 2));
-      callback(options);
+      options.message = `I found some space, look at these: \n\n`;
+      meetings.forEach((meeting) => {
+        meesage += commons.parseDate(meeting.meetingTimeSlot.start.dateTime) + '-' +
+                commons.parseDate(meeting.meetingTimeSlot.end.dateTime) + '\n\n';
+      });
     }
-
+    options.speech = 'I found some space, look at these';
+    console.log('findMeetingTimes.options : ' + JSON.stringify(options, null, 2));
+    callback(options);
   });
-
 }
 
 exports.findMeetingTimes = findMeetingTimes;
