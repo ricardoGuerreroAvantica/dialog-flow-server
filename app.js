c03d772acaa6b63e6e5df9616b2809b88af3b124
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2018-07-31 13:55:11 -0600

    no message

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use('/', routes);
 //   next(err);
 // });
 
-if (app.get('env') === 'development') {
+/* if (app.get('env') === 'development') {
   app.use(function (err, req, res) {
     res.status(err.status || 500);
     return res.json('error', {
@@ -49,7 +49,7 @@ app.use(function (err, req, res) {
     message: err.message,
     error: {}
   });
-});
+}); */
 
 module.exports = app;
 
