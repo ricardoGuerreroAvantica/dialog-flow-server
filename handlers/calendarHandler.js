673e4556cd551296ed6a6d67255123fe11dec74e
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2018-09-10 17:21:10 -0600

    test invite user

--- handlers/calendarHandler.js
+++ handlers/calendarHandler.js
@@ -57,6 +57,7 @@ function scheduleMeeting(options, callback){
 
 
 function findMeetingTimes(options, callback){
+  Console.log(JSON.stringify(options));
   if (options.message == ""){
     var parameters = options.parameters;
     var duration = parameters.duration;
