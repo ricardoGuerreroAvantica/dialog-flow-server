035195e8a87ed3cf06d4b8606a51166cd83b5a97
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2018-09-11 13:59:25 -0600

    no message

--- handlers/calendarHandler.js
+++ handlers/calendarHandler.js
@@ -57,85 +57,48 @@ function scheduleMeeting(options, callback){
 
 
 function findMeetingTimes(options, callback){
-  if (options.message =! ""){
-      console.log("Enter the find meeting times" + options.message)
-      var parameters = options.parameters;
-      var duration = parameters.duration;
-      var date = parameters.date;
-      var time = parameters.time;
-      var user = options.user;
-      var postBody = {
-        attendees: commons.getAttendees([user]),
-        timeConstraint : commons.getTimeConstraint(date, time),
-        meetingDuration : 'PT1H'
-      };
-
-      request.postData('graph.microsoft.com','/v1.0/me/findMeetingTimes', options.access_token, JSON.stringify(postBody), (error, response) => {
-        if (error){
-          //console.log('findMeetingTimes.error : ' + JSON.stringify(error));
-          errorHandler.actionError(error);
-        }
-
-        var meetings = response.meetingTimeSuggestions;
-        //console.log('findMeetingTimes.meetings : ' + JSON.stringify(meetings, null, 2));
-        if (meetings.length > 0){
-          options.message = options.speech = `I found some space, look at these: \n\n`;
-          options.message += '-----------------------' + '\n\n';
-          meetings.forEach((meeting) => {
-            options.message += commons.parseDate(meeting.meetingTimeSlot.start.dateTime) + ' - ' +
-                    commons.parseDate(meeting.meetingTimeSlot.end.dateTime) + '\n\n';
-          });
-          //console.log('findMeetingTimes.options : ' + JSON.stringify(options, null, 2));
-          callback(options);
-        }else{
-          //console.log('findMeetingTimes.meetings : empty response' );
-          options.message = options.speech = "Sorry couldn't find any space";
-          callback(options);
-        }
-        callback(options);
-      });
-    }
-  else{
-      callback(options);
-    }
-  }
-  
-  console.log("Enter the find meeting times" + options.message)
-  var parameters = options.parameters;
-  var duration = parameters.duration;
-  var date = parameters.date;
-  var time = parameters.time;
-  var user = options.user;
-  var postBody = {
-    attendees: commons.getAttendees([user]),
-    timeConstraint : commons.getTimeConstraint(date, time),
-    meetingDuration : 'PT1H'
-  };
-
-  request.postData('graph.microsoft.com','/v1.0/me/findMeetingTimes', options.access_token, JSON.stringify(postBody), (error, response) => {
-    if (error){
-      //console.log('findMeetingTimes.error : ' + JSON.stringify(error));
-      errorHandler.actionError(error);
-    }
-
-    var meetings = response.meetingTimeSuggestions;
-    //console.log('findMeetingTimes.meetings : ' + JSON.stringify(meetings, null, 2));
-    if (meetings.length > 0){
-      options.message = options.speech = `I found some space, look at these: \n\n`;
-      options.message += '-----------------------' + '\n\n';
-      meetings.forEach((meeting) => {
-        options.message += commons.parseDate(meeting.meetingTimeSlot.start.dateTime) + ' - ' +
-                commons.parseDate(meeting.meetingTimeSlot.end.dateTime) + '\n\n';
-      });
-      //console.log('findMeetingTimes.options : ' + JSON.stringify(options, null, 2));
-      callback(options);
-    }else{
-      //console.log('findMeetingTimes.meetings : empty response' );
-      options.message = options.speech = "Sorry couldn't find any space";
-      callback(options);
-    }
+  console.log("el mensaje " + options.message)
+  if (options.message == ""){
     callback(options);
-  });
+    console.log("Enter the find meeting times" + options.message)
+    var parameters = options.parameters;
+    var duration = parameters.duration;
+    var date = parameters.date;
+    var time = parameters.time;
+    var user = options.user;
+    var postBody = {
+      attendees: commons.getAttendees([user]),
+      timeConstraint : commons.getTimeConstraint(date, time),
+      meetingDuration : 'PT1H'
+    };
+
+    request.postData('graph.microsoft.com','/v1.0/me/findMeetingTimes', options.access_token, JSON.stringify(postBody), (error, response) => {
+      if (error){
+        //console.log('findMeetingTimes.error : ' + JSON.stringify(error));
+        errorHandler.actionError(error);
+      }
+
+      var meetings = response.meetingTimeSuggestions;
+      //console.log('findMeetingTimes.meetings : ' + JSON.stringify(meetings, null, 2));
+      if (meetings.length > 0){
+        options.message = options.speech = `I found some space, look at these: \n\n`;
+        options.message += '-----------------------' + '\n\n';
+        meetings.forEach((meeting) => {
+          options.message += commons.parseDate(meeting.meetingTimeSlot.start.dateTime) + ' - ' +
+                  commons.parseDate(meeting.meetingTimeSlot.end.dateTime) + '\n\n';
+        });
+        //console.log('findMeetingTimes.options : ' + JSON.stringify(options, null, 2));
+        callback(options);
+      }else{
+        //console.log('findMeetingTimes.meetings : empty response' );
+        options.message = options.speech = "Sorry couldn't find any space";
+        callback(options);
+      }
+      callback(options);
+    });
+  }
+  callback(options);
+
 }
 
 
