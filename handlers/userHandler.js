a056de87e5a35c30a5b467c9829b1deac141879c
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2018-09-14 11:43:41 -0600

    no message

--- handlers/userHandler.js
+++ handlers/userHandler.js
@@ -35,7 +35,7 @@ function searchUser(next, options, callback){
       console.log("Sorry i couldn't find any user with this discription: \n");
       console.log(userData.name ? userData.name : "Empty");
       console.log(userData.mail ? ("\nemail: " + userData.mail) : "Empty");
-      options.message = ("Sorry i couldn't find any user with this discription: \n") + (userData.name ? ("Name: " + userData.name) : "Empty") +(" " + userData.lastname ? (userData.lastname)  : "") +"\nEmail: "+ (userData.mail ? (userData.mail) : "Empty");
+      options.message = ("Sorry i couldn't find any user with this discription: \n") + (userData.name ? ("Name: " + userData.name) : "Empty") + " " +(userData.lastname ? (userData.lastname)  : "") +"\nEmail: "+ (userData.mail ? (userData.mail) : "Empty");
       callback(options);
     }
     if (response.data.value.length > 1){
