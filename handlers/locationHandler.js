feb1f9004af3bec81cd7e64233087e7b7872a40e
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2018-09-17 15:08:07 -0600

    no message

--- handlers/locationHandler.js
+++ handlers/locationHandler.js
@@ -12,14 +12,14 @@ function showLocations(options, callback){
   .then((response) => {
     if (response.data.value.length > 0){
       var locations = response.data.value;
-      options.message = options.speech = "Found these locations: \n";
-      options.message += '-----------------------' + '\n';
+      options.message = options.speech = "Found these locations: \n\n";
+      options.message += '-----------------------' + '\n\n';
       locations.forEach((location) => {
-        message += location.name + '\n';
+        message += location.name + '\n\n';
       });
       callback(options);
     }else{
-      options.message = options.speech = "There aren't any location available \n";
+      options.message = options.speech = "There aren't any location available \n\n";
       callback(options);
     }
   })
