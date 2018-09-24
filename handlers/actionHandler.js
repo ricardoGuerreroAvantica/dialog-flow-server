4809da0e2317ca25999ad883c7509717801e87bc
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2018-09-24 09:49:16 -0600

    no message

--- handlers/actionHandler.js
+++ handlers/actionHandler.js
@@ -41,11 +41,7 @@ function parseAction(req, res, callback){
       Action.prototype.findMeetingTimes = calendarHandler.findMeetingTimes;
       //PRE
       Action.pre('findMeetingTimes', authenticate.refreshToken)
-      .pre('findMeetingTimes', userHandler.searchUser)
-      .pre('findMeetingTimes', calendarHandler.PrefindMeetingTimes)
-      .pre('findMeetingTimes', calendarHandler.PrefindMeetingTimes)
-      .pre('findMeetingTimes', calendarHandler.PrefindMeetingTimes)
-      .pre('findMeetingTimes', calendarHandler.PrefindMeetingTimes);
+      .pre('findMeetingTimes', userHandler.searchUser);
 
 
       console.log("Procced to check if user is available")
