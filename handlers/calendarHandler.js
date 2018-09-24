c8f662ee98798613cd5ff1d4af60386befe30471
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2018-09-24 13:32:34 -0600

    no message

--- handlers/calendarHandler.js
+++ handlers/calendarHandler.js
@@ -66,7 +66,7 @@ function PrefindMeetingTimes(next, options, callback){
 
   //Checks if the message is empty, then generate the standard message title:
   if(options.message == ""){
-    options.message += options.speech = `Is available at: \n\n`;
+    options.message += options.speech = options.user.givenName `Is available at: \n\n`;
     options.message += '-----------------------' + '\n\n';
   }
 
@@ -97,7 +97,7 @@ function PrefindMeetingTimes(next, options, callback){
         options.message += commons.parseDate(meeting.meetingTimeSlot.start.dateTime) + ' - ' +
                 commons.parseDate(meeting.meetingTimeSlot.end.dateTime) + '\n\n';
       });
-      console.log("Meeetings: " +JSON.stringify(meetings));
+      console.log("Meeetings: " + JSON.stringify(meetings));
       next(options,callback);
     }else{
       if (options.message != ""){
