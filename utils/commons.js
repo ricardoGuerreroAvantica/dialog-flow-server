528df17a8ebb14010274e30e7cd2747cd75cfda5
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2018-09-26 11:17:43 -0600

    no message

--- utils/commons.js
+++ utils/commons.js
@@ -38,8 +38,8 @@ function getTimeConstraint(date, time, timeMargin){
   console.log("START TIMER: " + date + 'T' + startTime);
   console.log("END TIMER: " + date + 'T' + endTime);
 
-  var startDate = date + 'T' + time + '.000Z';
-  var endDate = date + 'T' + endTime + '.000Z';
+  var startDate = date + 'T' + time ;
+  var endDate = date + 'T' + endTime ;
 
   var result = {
     "timeslots": [
