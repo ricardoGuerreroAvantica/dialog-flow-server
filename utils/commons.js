8b5556e27c338f9610b23797e4310ae3e15417a4
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2018-10-04 18:30:41 -0600

    no message

--- utils/commons.js
+++ utils/commons.js
@@ -38,9 +38,10 @@ function getAttendees(invites){
 }
 
 
-function getTimeConstraint(date, startTimeMargin, endTimeMargin){
+function getTimeConstraint(date, time, startTimeMargin, endTimeMargin){
 
-  console.log("commons.getTimeConstraint.date :" +date);
+
+  console.log("commons.getTimeConstraint.date :" +date+time);
   var newdate   = moment(date, 'YYYY-MM-DDThh:mm:ss.SSS').add(6, 'hours').format('YYYY-MM-DDTHH:mm:ss.000') + 'Z' ;
   var endDate   = moment(newdate, 'YYYY-MM-DDThh:mm:ss.SSS').add(startTimeMargin, 'hours').format('YYYY-MM-DDTHH:mm:ss.000') + 'Z' ;
   var startDate = moment(newdate, 'YYYY-MM-DDThh:mm:ss.SSS').subtract(endTimeMargin, 'hours').format('YYYY-MM-DDTHH:mm:ss.000') + 'Z' ;
