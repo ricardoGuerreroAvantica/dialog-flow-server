6c5a28281b19ca613386f4212a307a5a9bbd8a73
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2018-11-09 14:23:40 -0600

    no message

--- handlers/userHandler.js
+++ handlers/userHandler.js
@@ -43,7 +43,7 @@ function preSearchUser(next, options, callback){
         next(options, callback);
       }
       if (response.data.value.length > 1){
-        options.message = "There is more than one employee with this description, maybe you are searching for:\n¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯n"
+        options.message = "There is more than one employee with this description, maybe you are searching for:\n¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\n"
         for(i = 0; i < response.data.value.length; i++ ){
           console.log(response.data.value[i])
           if ( i!= response.data.value.length-1){
