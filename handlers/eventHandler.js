8c792d232e64f28b2eaf6c53685b17b7d4df48dc
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2019-01-07 09:16:56 -0600

    no message

--- handlers/eventHandler.js
+++ handlers/eventHandler.js
@@ -3,7 +3,7 @@ var commons = require('../utils/commons.js');
 
 
 function inviteUser(options, callback){
-  console.log("INVITE CONTEXT : "+options.contexts);
+  console.log("INVITE CONTEXT : "+JSON.stringify(options.contexts));
   if (options.message == ""){
     var user = options.user;
     var invite = { "emailAddress": { "address":user.mail, "name": user.displayName }, "type": "required" }
