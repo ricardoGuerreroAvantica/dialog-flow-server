6a57c01ca4d8be142005f218874aed92baf668b3
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2019-01-08 11:43:52 -0600

    no message

--- handlers/userHandler.js
+++ handlers/userHandler.js
@@ -17,7 +17,7 @@ function preSearchUser(next, options, callback){
                   ((parameters.secondName) ? (unescape(encodeURIComponent(" " + userData.secondName))) : null)+
                   ((parameters.lastname) ? (unescape(encodeURIComponent(" " + userData.lastname))) : null)+
                   ((parameters.secondLastname) ? (unescape(encodeURIComponent(" " +userData.secondLastname))) : null)).trim()
-                  +"')")).replace(/\s+/g,' ').trim()
+                  +"')")).replace(/\s+/g," ").trim()
     var url = 'https://graph.microsoft.com/v1.0/users?$filter=';
     console.log("preSearchUser.graph filter: "+url+filter)
     axios.get(url + filter, {
