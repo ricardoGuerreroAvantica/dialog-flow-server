fab11059e806ed255011a40ce082003fe1c8f927
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2019-01-16 16:53:31 -0600

    ACB-88:Improve the application timezone handle

--- microsoftGraph/authenticate.js
+++ microsoftGraph/authenticate.js
@@ -116,7 +116,7 @@ function signIn(req, res){
   }
   if (reqJSONBody.state=="IOS"){
     UserTimezone=commons.getTimeZone(access_token);
-    tokens[reqJSONBody.session_state] = { ACCESS_TOKEN_CACHE_KEY : reqJSONBody.token_body, REFRESH_TOKEN_CACHE_KEY : "" ,TIMEZONE:UserTimezone}
+    tokens[reqJSONBody.session_state] = { TIMEZONE:UserTimezone, ACCESS_TOKEN_CACHE_KEY : reqJSONBody.token_body, REFRESH_TOKEN_CACHE_KEY : ""}
     console.log(JSON.stringify("IOS session: is " + json.stringify(tokens[reqJSONBody.session_state])))
 
     return res.json({ response : { description : "Login Successful in ios mobile" } });
@@ -133,7 +133,7 @@ function signIn(req, res){
     getTokenFromCode(code, (error, access_token, refresh_token, sessionId) => {
       if (!error) {
         UserTimezone=commons.getTimeZone(access_token);
-        tokens[state] = { ACCESS_TOKEN_CACHE_KEY : access_token, REFRESH_TOKEN_CACHE_KEY : refresh_token ,TIMEZONE:UserTimezone}
+        tokens[state] = {TIMEZONE:UserTimezone, ACCESS_TOKEN_CACHE_KEY : access_token, REFRESH_TOKEN_CACHE_KEY : refresh_token}
         console.log(JSON.stringify("skype session: is " + JSON.stringify(tokens[state])))
         return res.sendFile(__dirname + '/signIn.html');
       }else{
