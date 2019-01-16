5f91d18f727c51840eea0522f775ecc2cc6b8fd8
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2019-01-16 16:02:58 -0600

    ACB-88:Improve the application timezone handle

--- utils/commons.js
+++ utils/commons.js
@@ -102,7 +102,7 @@ function getTimeZone(access_token){
     if (response.data.value.length != 0){
       console.log("TIMEZONE RESPONSES"+JSON.stringify(response.data.value))
       for (i = 0; i < timezones.timezones.length; i++) {
-        console.log(timezones.timezones[i].name);
+        console.log(timezones.timezones[i].name.trim() == response.data.value.trim() + " "+timezones.timezones[i].name.trim());
         if(timezones.timezones[i].name == response.data.value){
           console.log(timezones.timezones[i].name +timezones.timezones[i].time)
         }
