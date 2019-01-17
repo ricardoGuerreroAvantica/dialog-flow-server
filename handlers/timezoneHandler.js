1f5f2b786f813285fc42797f319d2e959cc5da97
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2019-01-17 13:29:07 -0600

    no message

--- handlers/timezoneHandler.js
+++ handlers/timezoneHandler.js
@@ -2,6 +2,7 @@ var timezones = require('./../constants/Timezones.js');
 var axios = require('axios');
 
 function getTimeZone(next, options, callback){
+    console.log("Enter");
     axios.get("https://graph.microsoft.com/v1.0/me/mailboxSettings/timeZone", {
       headers : {
         'Content-Type': 
@@ -14,6 +15,8 @@ function getTimeZone(next, options, callback){
     
       if (response.data.value.length != 0){
         for (i = 0; i < timezones.timezones.length; i++) {
+            console.log("PASS : "+timezones.timezones[i].name+" "+timezones.timezones[i].time)
+
           if(timezones.timezones[i].name == response.data.value){
             console.log("Selected : "+timezones.timezones[i].name+" "+timezones.timezones[i].time)
             console.log(JSON.stringify({timezone:timezones.timezones[i].name,time:parseInt(timezones.timezones[i].time)}))
