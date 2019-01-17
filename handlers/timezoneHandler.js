87b00c23d27ee2846a1ee800a8ef9fb9f6aed6ee
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2019-01-17 13:34:41 -0600

    no message

--- handlers/timezoneHandler.js
+++ handlers/timezoneHandler.js
@@ -3,30 +3,37 @@ var axios = require('axios');
 
 function getTimeZone(next, options, callback){
     console.log("Enter");
-    axios.get("https://graph.microsoft.com/v1.0/me/mailboxSettings/timeZone", {
-      headers : {
-        'Content-Type': 
-        'application/json',
-        Accept: 'application/json',
-        Authorization: 'Bearer ' + options.access_token
-      }
-    })
-    .then((response) => {
-    
-      if (response.data.value.length != 0){
-        for (i = 0; i < timezones.timezones.length; i++) {
-            console.log("PASS : "+timezones.timezones[i].name+" "+timezones.timezones[i].time)
-
-          if(timezones.timezones[i].name == response.data.value){
-            console.log("Selected : "+timezones.timezones[i].name+" "+timezones.timezones[i].time)
-            console.log(JSON.stringify({timezone:timezones.timezones[i].name,time:parseInt(timezones.timezones[i].time)}))
-            options.userTimezone= {timezone:timezones.timezones[i].name,time:parseInt(timezones.timezones[i].time)};
-            next(options, callback);
-          }
+    try{
+        axios.get("https://graph.microsoft.com/v1.0/me/mailboxSettings/timeZone", {
+        headers : {
+            'Content-Type': 
+            'application/json',
+            Accept: 'application/json',
+            Authorization: 'Bearer ' + options.access_token
         }
-      }
-      })
-      next(options, callback);
+        })
+        .then((response) => {
+        
+        if (response.data.value.length != 0){
+            for (i = 0; i < timezones.timezones.length; i++) {
+                console.log("PASS : "+timezones.timezones[i].name+" "+timezones.timezones[i].time)
+
+            if(timezones.timezones[i].name == response.data.value){
+                console.log("Selected : "+timezones.timezones[i].name+" "+timezones.timezones[i].time)
+                console.log(JSON.stringify({timezone:timezones.timezones[i].name,time:parseInt(timezones.timezones[i].time)}))
+                options.userTimezone= {timezone:timezones.timezones[i].name,time:parseInt(timezones.timezones[i].time)};
+                next(options, callback);
+            }
+            }
+        }
+        })
+        next(options, callback);
+        }
+        catch
+        {
+            console.error()
+        }
+        next(options, callback);
   }
 
 
