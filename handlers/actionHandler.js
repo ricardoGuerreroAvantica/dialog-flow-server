992994961e0f36c612b75a34ac8b4076cea012c9
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2019-01-18 10:01:44 -0600

    ACB-88:Improve the application timezone handle

--- handlers/actionHandler.js
+++ handlers/actionHandler.js
@@ -21,6 +21,8 @@ function parseAction(req, res, callback){
     //Case: calendar_user_available
     //Description: This case is trigger when the user ask for "Is [name] available [date]", search if the employee exists and then proceed to show the disponibility of them.
     case 'calendar_user_available' :
+    console.log("DATA:  # 1")
+
       Action.prototype.checkMeetingTimes = calendarHandler.checkMeetingTimes;
       Action.pre('checkMeetingTimes', authenticate.refreshToken)
       .pre('checkMeetingTimes', timezoneHandler.getTimeZone)
@@ -34,12 +36,16 @@ function parseAction(req, res, callback){
     //Case: helper
     //Description: This case is trigger when the user ask for "Help" 
     case 'helper' :
+    console.log("DATA:  # 2")
+
       userHandler.helper(options, callback);
       break;
 
     //Case: show_events
     //Description: This case is trigger when the user ask for "Show my events" 
     case 'show_events' :
+    console.log("DATA:  # 3")
+
       Action.prototype.showEvents = calendarHandler.showEvents;
       Action.pre('showEvents', authenticate.refreshToken)
       .pre('showEvents', timezoneHandler.getTimeZone)      ;
@@ -50,6 +56,8 @@ function parseAction(req, res, callback){
     //Case: show_events_on_date
     //Description: This case is trigger when the user ask for "Show my events for [date]"
     case 'show_events_on_date' :
+    console.log("DATA:  # 4")
+
       Action.prototype.showEventsOnDate = calendarHandler.showEventsOnDate;
       Action.pre('showEventsOnDate', authenticate.refreshToken)
       .pre('showEventsOnDate', timezoneHandler.getTimeZone)
@@ -61,6 +69,8 @@ function parseAction(req, res, callback){
     //Case: show_locations
     //Description: #Currently the bot dont handle locations.
     case 'show_locations' :
+    console.log("DATA:  # 5")
+
       Action.prototype.showLocations = locationHandler.showLocations;
       Action.pre('showLocations', authenticate.refreshToken)
       .pre('showLocations', timezoneHandler.getTimeZone)
@@ -72,6 +82,8 @@ function parseAction(req, res, callback){
     //Case: create_event_finish
     //Description: This case is trigger when the user ask for "Done" and proceed to create the event in their calendars
     case 'create_event_finish' :
+    console.log("DATA:  # 6")
+
       Action.prototype.scheduleMeeting = calendarHandler.scheduleMeeting;
       Action.pre('scheduleMeeting', authenticate.refreshToken)
       .pre('scheduleMeeting', timezoneHandler.getTimeZone)
@@ -82,6 +94,8 @@ function parseAction(req, res, callback){
     //Case: create_event_invite
     //Description: This case is trigger when the user ask for "invite [name]"
     case 'create_event_invite' :
+    console.log("DATA:  # 7")
+
       Action.prototype.inviteUser = eventHandler.inviteUser;
       Action.pre('inviteUser', authenticate.refreshToken)
         .pre('inviteUser',userHandler.preSearchUser);
@@ -92,18 +106,23 @@ function parseAction(req, res, callback){
     //Case: create_event_uninvite
     //Description: This case is trigger when the user ask for "Uninvite [name]"
     case 'create_event_uninvite' :
+    console.log("DATA:  # 8")
+
       eventHandler.deleteInvite(options, callback);
       break;
     
     //Case: create_event_show_invites
     //Description: This case is trigger when the user ask for "Show my invites", and the bot proceed to show all the current invites for the event.
     case 'create_event_show_invites' :
+    console.log("DATA:  # 9")
       eventHandler.showInvites(options, callback);
       break;
 
     //Case: check_available_Only_name
     //Description: this case is trigger when the user search for a employee using only the name of the employee
     case 'check_available_Only_name' :
+    console.log("DATA:  # 10")
+
         Action.prototype.checkUser = userHandler.checkUser;
         //PRE
         Action.pre('checkUser', authenticate.refreshToken)
@@ -117,6 +136,8 @@ function parseAction(req, res, callback){
     //Case: Show_event_Info
     //Description: This case is trigger when the user ask for "Show my event info" request, and will show the user the event body of the current event creation
     case 'Show_event_Info' :
+    console.log("DATA:  # 11")
+
       options.simpleInfo = false;
       Action.prototype.showEventDetails = calendarHandler.showEventDetails;
         //PRE
@@ -129,6 +150,8 @@ function parseAction(req, res, callback){
     //Case: createEventBegin
     //Description: This case is trigger when all the information of the event creation is collected and then proced to show the event body to the user
     case 'createEventBegin' :
+    console.log("DATA:  # 12")
+
       options.simpleInfo = true;
       Action.prototype.showEventDetails = calendarHandler.showEventDetails;
         //PRE
@@ -141,6 +164,8 @@ function parseAction(req, res, callback){
     //Case: Default Answer
     //Description: This is the default answer sended when there is no other case that match the request case.
     default:
+    console.log("DATA:  # 13")
+
       this.options.message = 'Could you repeat that?';
       this.options.speech = 'Could you repeat that?';
       callback(this.options);
