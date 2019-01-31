f156eda0f021cddaed2d10b4f233342aed2cbef0
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2019-01-31 16:23:56 -0600

    fix on hours

--- constants/Timezones.js
+++ constants/Timezones.js
@@ -96,7 +96,7 @@ const timezones=[   {name:"Dateline Standard Time"                    ,time:"-12
                     {name:"Paraguay Standard Time"                    ,time:"-04"},
                     {name:"Paraguay Standard Time"                    ,time:"-04"},
                     {name:"Paraguay Standard Time"                    ,time:"-04"},
-                    {name:"Afghanistan Standard Time"                 ,time:"+4.30"},
+                    {name:"Afghanistan Standard Time"                 ,time:"+4.5"},
                     {name:"Kamchatka Standard Time"                   ,time:"+12"}];
 
 exports.timezones = timezones;
