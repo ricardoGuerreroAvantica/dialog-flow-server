caa66ee5fc417ec9667dc8c19dfdb2e1a03edec9
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2019-01-31 14:07:17 -0600

    refactor calendar handler

--- handlers/calendarHandler.js
+++ handlers/calendarHandler.js
@@ -95,8 +95,9 @@ async function scheduleMeeting(options){
     var name = replaceSpecialCharacteres(eventContext.parameters.eventName);
     var duration = eventContext.parameters.duration || {amount : 1, unit : 'hours'};
     var date = eventContext.parameters.date + ' ' + eventContext.parameters.time;
-    var startDate = moment.utc(date, 'YYYY-MM-DD HH:mm:ss').add(parseInt(options.userTimezone.time), 'hours').format('YYYY-MM-DDTHH:mm:ss');
-    var endDate = moment.utc(date, 'YYYY-MM-DD HH:mm:ss').add(parseInt(options.userTimezone.time), 'hours').add(duration.amount, duration.unit).format('YYYY-MM-DDTHH:mm:ss');
+    console.log()
+    var startDate = moment.utc(date, 'YYYY-MM-DDThh:mm:ss.SSS').add(parseInt(options.userTimezone.time), 'hours').format('YYYY-MM-DDTHH:mm:ss');
+    var endDate = moment.utc(date, 'YYYY-MM-DDThh:mm:ss.SSS').add(parseInt(options.userTimezone.time), 'hours').add(duration.amount, duration.unit).format('YYYY-MM-DDTHH:mm:ss');
     console.log("times: " +startDate+"   "+endDate)
     if (duration.unit === 'h') duration.unit = 'hours';
     else if(duration.unit === 'min') duration.unit = 'minutes';
