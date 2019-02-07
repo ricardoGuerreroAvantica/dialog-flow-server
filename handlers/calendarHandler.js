e648606be46f68926b406add6200c16149a155d3
Ricardo Guerrero Matus <ricardo.guerrero@avantica.net>
2019-02-07 09:20:42 -0600

    no message

--- handlers/calendarHandler.js
+++ handlers/calendarHandler.js
@@ -282,18 +282,18 @@ async function showEventsOnDate(options){
     console.log(options.userTimezone.time)
 
     console.log(date+('T00:00:00.000'))
-    console.log(moment((date+('T00:00:00.000')), 'YYYY-MM-DDThh:mm:ss.SSS').format('YYYY-MM-DDThh:mm:ss.SSS'))
-    console.log(moment((date+('T00:00:00.000')), 'YYYY-MM-DDThh:mm:ss.SSS').add(parseFloat(options.userTimezone.time), 'hours'))
+    console.log(moment((date+('T00:00:00.000')), 'YYYY-MM-DDTHH:mm:ss.SSS').format('YYYY-MM-DDThh:mm:ss.SSS'))
+    console.log(moment((date+('T00:00:00.000')), 'YYYY-MM-DDTHH:mm:ss.SSS').add(parseFloat(options.userTimezone.time), 'hours'))
     console.log("------------")
     console.log((date+('T23:59:59.000')))
     console.log(moment((date+('T23:59:59.000')), 'YYYY-MM-DDTHH:mm:ss.SSS').format('YYYY-MM-DDTHH:mm:ss.SSS'))
-    console.log(moment((date+('T23:59:59.000')), 'YYYY-MM-DDThh:mm:ss.SSS').format('YYYY-MM-DDThh:mm:ss.SSS'))
+    console.log(moment((date+('T23:59:59.000')), 'YYYY-MM-DDTHH:mm:ss.SSS').format('YYYY-MM-DDThh:mm:ss.SSS')).add(parseFloat(options.userTimezone.time), 'hours')
     console.log("------------")
 
-    console.log(moment((date+('T23:59:59.000')), 'YYYY-MM-DDTHH:mm:ss.SSS').add(parseFloat(options.userTimezone.time), 'hours').format('YYYY-MM-DDThh:mm:ss.SSS'))
+    console.log(moment((date+('T23:59:59.000')), 'YYYY-MM-DDTHH:mm:ss.SSS').add(parseFloat(options.userTimezone.time), 'hours').format('YYYY-MM-DDTHH:mm:ss.SSS'))
 
-    var startDate=moment((date+('T00:00:00.000')), 'YYYY-MM-DDThh:mm:ss.SSS').add(parseFloat(options.userTimezone.time), 'hours').format('YYYY-MM-DDThh:mm:ss.SSS');
-    var endDate=moment((date+('T23:59:59.000')), 'YYYY-MM-DDThh:mm:ss.SSS').add(parseFloat(options.userTimezone.time), 'hours').format('YYYY-MM-DDThh:mm:ss.SSS');
+    var startDate=moment((date+('T00:00:00.000')), 'YYYY-MM-DDThh:mm:ss.SSS').add(parseFloat(options.userTimezone.time), 'hours').format('YYYY-MM-DDTHH:mm:ss.SSS');
+    var endDate=moment((date+('T23:59:59.000')), 'YYYY-MM-DDThh:mm:ss.SSS').add(parseFloat(options.userTimezone.time), 'hours').format('YYYY-MM-DDTHH:mm:ss.SSS');
     filter = 'startdatetime=' + startDate+ 'Z' +
               '&enddatetime=' + endDate+ 'Z';
     url = 'https://graph.microsoft.com/v1.0/me/calendarview?';
