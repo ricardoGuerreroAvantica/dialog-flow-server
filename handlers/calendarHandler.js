6597d6b12025614535c0d912bc006dc343f62bc9
Ricardo Guerrero Matus <ricardo.guerrero@avantica.net>
2019-02-19 09:58:48 -0600

    no message

--- handlers/calendarHandler.js
+++ handlers/calendarHandler.js
@@ -105,7 +105,7 @@ async function scheduleMeeting(options){
           }
           try{
             console.log("response"+JSON.stringify(response))
-            generateEventBody(response.subject,options.userTimezone,response.start.date,response.end.dateTime,response.organizer.emailAddress.name)
+            generateEventBody(response.subject,options.userTimezone,response.start.dateTime,response.end.dateTime,response.organizer.emailAddress.name)
           }
           catch(err){
             console.log("the error: " + err)
